d32cdb6994983280bd8fa3e783d51f180c8a7e53
rfashwal <rfashwal@hotmail.com>
2018-10-19 16:12:22 +0300

    initial commit

---
+++ build/precache-manifest.0dd0b4d6d0f8badafb6acb6922e5a48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c04e9001b2412db6fc49",
+    "url": "/static/js/main.c04e9001.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c04e9001b2412db6fc49",
+    "url": "/static/css/main.51321404.chunk.css"
+  },
+  {
+    "revision": "30cac2894d4be577bf044309ab2cdcad",
+    "url": "/index.html"
+  }
+];
