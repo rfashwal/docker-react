02010315d000c357d108a16ddecd30c68852b84d
rfashwal <rfashwal@hotmail.com>
2018-10-19 18:29:30 +0300

    add feature

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             <i>Hi there!</i>
+            <p>I am a new Feature</p>
           </p>
           <a
             className="App-link"
